aaa140d4ab87b09b2dc6af2d5360d4e83574275d
edemdj <edemdj.edemdjossou1997@gmail.com>
2025-06-11 18:09:29 +0200

    UTF-8 Validation

---
+++ utf8_validation/0-validate_utf8.py
@@ -0,0 +1,26 @@
+def validUTF8(data):
+    num_bytes = 0
+
+    for byte in data:
+        # We only care about the least significant 8 bits
+        byte = byte & 0xFF
+
+        if num_bytes == 0:
+            # Count leading 1's to determine byte length
+            if byte >> 7 == 0:
+                continue  # 1-byte character
+            elif byte >> 5 == 0b110:
+                num_bytes = 1
+            elif byte >> 4 == 0b1110:
+                num_bytes = 2
+            elif byte >> 3 == 0b11110:
+                num_bytes = 3
+            else:
+                return False
+        else:
+            # Continuation byte must start with 10
+            if byte >> 6 != 0b10:
+                return False
+            num_bytes -= 1
+
+    return num_bytes == 0
